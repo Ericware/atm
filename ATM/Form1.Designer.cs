de6eb1d5d905e2dd3274cd33eb719714d4597aff
T-tune <kitoware@gmail.com>
2022-03-15 13:41:42 -0600

    Add files via upload

---
+++ ATM/Form1.Designer.cs
@@ -0,0 +1,83 @@
+﻿
+namespace ATM
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.UserNameBox = new System.Windows.Forms.TextBox();
+            this.PasswordBox = new System.Windows.Forms.TextBox();
+            this.LogginBtn = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // UserNameBox
+            // 
+            this.UserNameBox.Location = new System.Drawing.Point(351, 94);
+            this.UserNameBox.Name = "UserNameBox";
+            this.UserNameBox.Size = new System.Drawing.Size(100, 20);
+            this.UserNameBox.TabIndex = 0;
+            // 
+            // PasswordBox
+            // 
+            this.PasswordBox.Location = new System.Drawing.Point(351, 145);
+            this.PasswordBox.Name = "PasswordBox";
+            this.PasswordBox.Size = new System.Drawing.Size(100, 20);
+            this.PasswordBox.TabIndex = 1;
+            // 
+            // LogginBtn
+            // 
+            this.LogginBtn.Location = new System.Drawing.Point(339, 244);
+            this.LogginBtn.Name = "LogginBtn";
+            this.LogginBtn.Size = new System.Drawing.Size(112, 37);
+            this.LogginBtn.TabIndex = 2;
+            this.LogginBtn.Text = "button1";
+            this.LogginBtn.UseVisualStyleBackColor = true;
+            this.LogginBtn.Click += new System.EventHandler(this.LogginBtn_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.LogginBtn);
+            this.Controls.Add(this.PasswordBox);
+            this.Controls.Add(this.UserNameBox);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox UserNameBox;
+        private System.Windows.Forms.TextBox PasswordBox;
+        private System.Windows.Forms.Button LogginBtn;
+    }
+}
+
